ed1e76523530444c765d773443d6da1c1e3c1fce
SoRa <paradorn3237@gmail.com>
2022-12-08 01:39:11 +0700

    .

--- Joke-rust/src/main.rs
+++ Joke-rust/src/main.rs
@@ -1,11 +1,14 @@
 use std::{ops::Index, io::stdin};
+// import Standard libray
 
+//mainFunction
 fn main() {
     dose();
     input_str();
     Sec_str();
 }
 
+//Love rust function
 fn dose() {
     let mut x = 0;
     loop {
@@ -16,12 +19,16 @@ fn dose() {
       }
     } 
 }
+
+//what is your name
 fn input_str() {
   let mut line = String::new();
   println!("Enter your name :");
   std::io::stdin().read_line(&mut line).unwrap();
   println!("Hello , {}", line);
 }
+
+//how are you
 fn Sec_str() {
     let mut fine =String::new();
     println!("1=Fine : 2=good : 3=bad");
@@ -29,3 +36,40 @@ fn Sec_str() {
     std::io::stdin().read_line(&mut fine).unwrap();
     println!("Hello , {}", fine);
   }
+
+
+  //
+  //
+  //
+  //
+  //
+  //
+  ///
+  /// //
+  /// //
+  /// /
+  /// /
+  /// /
+  /// //
+  /// /
+  /// 
+  /// /
+  /// /
+  /// /
+  /// /
+  /// /
+  /// /
+  /// /
+  /// /
+  /// /
+  /// /
+  /// 
+  /// /
+  /// //
+  /// 
+  /// //
+  /// //
+  /// /
+  /// //
+  /// /
+  /// 
